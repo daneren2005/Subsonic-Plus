a203dcd2203e10a8cc92d556609da3cc0fb2f620
sindre_mehus <sindre_mehus@104adb81-38d2-4886-885e-82898e39ce25>
2012-09-26 13:56:45 +0000

    Android: Added actionbar compat example.
    git-svn-id: svn://svn.code.sf.net/p/subsonic/code/trunk@3118 104adb81-38d2-4886-885e-82898e39ce25

---
+++ subsonic-android/actionbar/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+D:\NotEncrypted\subsonic\subsonic-android\actionbar\bin\AndroidManifest.xml : \
+D:\NotEncrypted\subsonic\subsonic-android\actionbar\AndroidManifest.xml \
---
+++ subsonic-android/actionbar/bin/subsonic-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+D:\NotEncrypted\subsonic\subsonic-android\actionbar\bin\subsonic-debug-unaligned.apk : \
+D:\NotEncrypted\subsonic\subsonic-android\actionbar\bin\subsonic.ap_ \
+D:\NotEncrypted\subsonic\subsonic-android\actionbar\bin\classes.dex \
+c:\Program Files (x86)\Android\android-sdk\tools\support\annotations.jar \
