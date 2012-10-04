57af47146830e9ffd81d282f8469ccb978c6d9e5
sindre_mehus <sindre_mehus@104adb81-38d2-4886-885e-82898e39ce25>
2012-10-04 08:07:04 +0000

    Android: Deleted actionbar example
    git-svn-id: svn://svn.code.sf.net/p/subsonic/code/trunk@3198 104adb81-38d2-4886-885e-82898e39ce25

--- subsonic-android/actionbar/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-D:\NotEncrypted\subsonic\subsonic-android\actionbar\bin\AndroidManifest.xml : \
-D:\NotEncrypted\subsonic\subsonic-android\actionbar\AndroidManifest.xml \
--- subsonic-android/actionbar/gen/com/example/android/actionbarcompat/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-/** Automatically generated file. DO NOT MODIFY */
-package com.example.android.actionbarcompat;
-
-public final class BuildConfig {
-    public final static boolean DEBUG = true;
-}
