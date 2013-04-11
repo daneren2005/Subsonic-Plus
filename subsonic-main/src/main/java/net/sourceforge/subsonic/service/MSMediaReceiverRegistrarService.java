a1958fd94fa6f4c18ad7538363a641f188e944cb
sindre_mehus <sindre_mehus@104adb81-38d2-4886-885e-82898e39ce25>
2013-04-11 20:35:59 +0000

    Commented out experimental UPnP support.
    git-svn-id: svn://svn.code.sf.net/p/subsonic/code/trunk@3399 104adb81-38d2-4886-885e-82898e39ce25

--- subsonic-main/src/main/java/net/sourceforge/subsonic/service/MSMediaReceiverRegistrarService.java
+++ subsonic-main/src/main/java/net/sourceforge/subsonic/service/MSMediaReceiverRegistrarService.java
@@ -1,10 +1,11 @@
 package net.sourceforge.subsonic.service;
 
-import org.fourthline.cling.support.xmicrosoft.AbstractMediaReceiverRegistrarService;
+//import org.fourthline.cling.support.xmicrosoft.AbstractMediaReceiverRegistrarService;
 
 /**
  * @author Sindre Mehus
  * @version $Id$
  */
-public class MSMediaReceiverRegistrarService extends AbstractMediaReceiverRegistrarService {
+//public class MSMediaReceiverRegistrarService extends AbstractMediaReceiverRegistrarService {
+public class MSMediaReceiverRegistrarService {
 }
