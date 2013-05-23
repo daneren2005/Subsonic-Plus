232247aa54d97dd5a81e436c7be3dcbf2f2de128
sindre_mehus <sindre_mehus@104adb81-38d2-4886-885e-82898e39ce25>
2013-05-23 16:42:49 +0000

    Android: Fixed video player bug.
    git-svn-id: svn://svn.code.sf.net/p/subsonic/code/trunk@3473 104adb81-38d2-4886-885e-82898e39ce25

--- subsonic-android/src/net/sourceforge/subsonic/androidapp/util/VideoPlayerType.java
+++ subsonic-android/src/net/sourceforge/subsonic/androidapp/util/VideoPlayerType.java
@@ -1,20 +1,20 @@
 /*
- This file is part of Subsonic.
-
- Subsonic is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
-
- Subsonic is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU General Public License for more details.
-
- You should have received a copy of the GNU General Public License
- along with Subsonic.  If not, see <http://www.gnu.org/licenses/>.
-
- Copyright 2009 (C) Sindre Mehus
+ * This file is part of Subsonic.
+ *
+ * Subsonic is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Subsonic is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Subsonic.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * Copyright 2013 (C) Sindre Mehus
  */
 package net.sourceforge.subsonic.androidapp.util;
 
@@ -44,7 +44,7 @@ public enum VideoPlayerType {
             if (!installedAd && !installedPro) {
                 new AlertDialog.Builder(activity)
                         .setMessage(R.string.video_get_mx_player_text)
-                        .setPositiveButton(R.string.video_get_mx_player_text, new DialogInterface.OnClickListener() {
+                        .setPositiveButton(R.string.video_get_mx_player_button, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int i) {
                                 try {
