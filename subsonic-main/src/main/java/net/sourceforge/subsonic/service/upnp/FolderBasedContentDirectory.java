9a20382127024681075a878a8e33d56fd263edfd
sindre_mehus <sindre_mehus@104adb81-38d2-4886-885e-82898e39ce25>
2014-05-27 15:11:34 +0000

    Fix DLNA recursion   https://github.com/daneren2005/Subsonic-Plus/commit/ca8a5345e9819d041f3cc32325dc83d455e2aa6d   http://forum.subsonic.org/forum/viewtopic.php?f=2&t=14045
    git-svn-id: svn://svn.code.sf.net/p/subsonic/code/trunk@4060 104adb81-38d2-4886-885e-82898e39ce25

--- subsonic-main/src/main/java/net/sourceforge/subsonic/service/upnp/FolderBasedContentDirectory.java
+++ subsonic-main/src/main/java/net/sourceforge/subsonic/service/upnp/FolderBasedContentDirectory.java
@@ -57,6 +57,7 @@ public class FolderBasedContentDirectory extends SubsonicContentDirectory {
     private static final Logger LOG = Logger.getLogger(FolderBasedContentDirectory.class);
     private static final String CONTAINER_ID_PLAYLIST_ROOT = "playlists";
     private static final String CONTAINER_ID_PLAYLIST_PREFIX = "playlist-";
+    private static final String CONTAINER_ID_FOLDER_PREFIX = "folder-";
     private MediaFileService mediaFileService;
     private PlaylistService playlistService;
 
@@ -90,7 +91,8 @@ public class FolderBasedContentDirectory extends SubsonicContentDirectory {
                 return browseFlag == BrowseFlag.METADATA ? browsePlaylistMetadata(playlist) : browsePlaylist(playlist, firstResult, maxResults);
             }
 
-            MediaFile mediaFile = mediaFileService.getMediaFile(Integer.parseInt(objectId));
+            int mediaFileId = Integer.parseInt(objectId.replace(CONTAINER_ID_FOLDER_PREFIX, ""));
+            MediaFile mediaFile = mediaFileService.getMediaFile(mediaFileId);
             return browseFlag == BrowseFlag.METADATA ? browseMediaFileMetadata(mediaFile) : browseMediaFile(mediaFile, firstResult, maxResults);
 
         } catch (Throwable x) {
@@ -225,7 +227,7 @@ public class FolderBasedContentDirectory extends SubsonicContentDirectory {
 
     private Container createContainer(MediaFile mediaFile) throws Exception {
         Container container = mediaFile.isAlbum() ? createAlbumContainer(mediaFile) : new MusicAlbum();
-        container.setId(String.valueOf(mediaFile.getId()));
+        container.setId(CONTAINER_ID_FOLDER_PREFIX + mediaFile.getId());
         container.setTitle(mediaFile.getName());
         List<MediaFile> children = mediaFileService.getChildrenOf(mediaFile, true, true, false);
         container.setChildCount(children.size());
