a6d104f8c8660e3dd2304aac81c8dc97f94e601b
Scott Jackson <daneren2005@gmail.com>
2014-08-27 14:40:36 -0700

    Sort playlists based off of name

--- subsonic-main/src/main/java/net/sourceforge/subsonic/dao/PlaylistDao.java
+++ subsonic-main/src/main/java/net/sourceforge/subsonic/dao/PlaylistDao.java
@@ -64,7 +64,14 @@ public class PlaylistDao extends AbstractDao {
         for (Playlist playlist : result3) {
             map.put(playlist.getId(), playlist);
         }
-        return new ArrayList<Playlist>(map.values());
+        List<Playlist> sortedPlaylists = new ArrayList<Playlist>(map.values());
+        Collections.sort(sortedPlaylists, new Comparator<Playlist>() {
+            @Override
+            public int compare(Playlist left, Playlist right) {
+                return left.getName().compareToIgnoreCase(right.getName());
+            }
+        });
+        return sortedPlaylists;
     }
 
     public List<Playlist> getWritablePlaylistsForUser(String username) {
