38bef169306ebf00fd0dc4b2f134b2eaa3f33a62
sindre_mehus <sindre_mehus@104adb81-38d2-4886-885e-82898e39ce25>
2015-02-13 17:47:39 +0000

    Disable DLNA and Sonos by default.
    git-svn-id: svn://svn.code.sf.net/p/subsonic/code/trunk@4386 104adb81-38d2-4886-885e-82898e39ce25

--- subsonic-main/src/main/java/net/sourceforge/subsonic/service/SettingsService.java
+++ subsonic-main/src/main/java/net/sourceforge/subsonic/service/SettingsService.java
@@ -201,9 +201,9 @@ public class SettingsService {
     private static final boolean DEFAULT_SORT_ALBUMS_BY_YEAR = true;
     private static final String DEFAULT_MEDIA_LIBRARY_STATISTICS = "0 0 0 0 0";
     private static final String DEFAULT_TRIAL_EXPIRES = null;
-    private static final boolean DEFAULT_DLNA_ENABLED = true;
+    private static final boolean DEFAULT_DLNA_ENABLED = false;
     private static final String DEFAULT_DLNA_SERVER_NAME = "Subsonic";
-    private static final boolean DEFAULT_SONOS_ENABLED = true;
+    private static final boolean DEFAULT_SONOS_ENABLED = false;
     private static final String DEFAULT_SONOS_SERVICE_NAME = "Subsonic";
     private static final int DEFAULT_SONOS_SERVICE_ID = 242;
 
