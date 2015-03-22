a794cdf82758d93f41fb70297394a719d9718861
sindre_mehus <sindre_mehus@104adb81-38d2-4886-885e-82898e39ce25>
2015-03-22 19:26:50 +0000

    Fixed bug when loading system avatars.
    git-svn-id: svn://svn.code.sf.net/p/subsonic/code/trunk@4452 104adb81-38d2-4886-885e-82898e39ce25

--- subsonic-main/src/main/java/net/sourceforge/subsonic/dao/schema/hsql/Schema35.java
+++ subsonic-main/src/main/java/net/sourceforge/subsonic/dao/schema/hsql/Schema35.java
@@ -138,7 +138,7 @@ public class Schema35 extends Schema {
 
             InputStream in = null;
             try {
-                in = getClass().getResourceAsStream(avatar + ".png");
+                in = getClass().getResourceAsStream("/net/sourceforge/subsonic/dao/schema/" + avatar + ".png");
                 byte[] imageData = IOUtils.toByteArray(in);
                 template.update("insert into system_avatar values (null, ?, ?, ?, ?, ?, ?)",
                                 new Object[]{avatar, new Date(), "image/png", 48, 48, imageData});
